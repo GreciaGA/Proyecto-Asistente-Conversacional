1fff8738a6c8a4e0324c92e1618ca61fd20c2b4c
GreciaGA <grecia.anicama@gmail.com>
2021-11-16 01:58:00 -0500

    subiendo humano digital3

---
+++ web/Contenido/js/tabs.js
@@ -0,0 +1,22 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+$(document).ready(function () {
+
+    $('#tab-container').easytabs({
+        animate: false
+    });
+
+    /*
+    $('.tab-container').bind('easytabs:after', function () {
+        $('#contentHolderEvento').perfectScrollbar({ suppressScrollX: true });
+        $('#contentHolderComunicado').perfectScrollbar({ suppressScrollX: true });
+    });
+
+    $('#contentHolderEvento').perfectScrollbar({ suppressScrollX: true });
+    $('#contentHolderComunicado').perfectScrollbar({ suppressScrollX: true });
+    */   
+});
